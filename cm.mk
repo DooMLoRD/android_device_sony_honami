55c106d364d4381f66df048bc2d81bd3a2d7a2f1
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-08-28 15:08:06 +0200

    togari: update fingerprint

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833_1271-0336/C6833:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833 BUILD_FINGERPRINT=Sony/C6833_1277-1824/C6833:4.2.2/14.1.B.1.493/Erp_jw:user/release-keys PRIVATE_BUILD_DESC="C6833-user 4.2.2 14.1.B.1.493 Erp_jw test-keys"
 
 PRODUCT_NAME := cm_togari
 PRODUCT_DEVICE := togari
